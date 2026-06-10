a5df5e347eab60d9a4a97d3526cb2d41ff479d8d
sivakrishna0 <sivachowz@icloud.com>
2026-06-10 22:25:42 +0530

    robol

---
+++ input/var.tf
@@ -0,0 +1,18 @@
+variable "cityName" {
+    type        = string
+    description = "The name of the city"
+    default     = "Bangalore"
+}
+
+output "cityName" {
+    value = var.cityName
+}
+
+variable "environment" {
+    type        = string
+    description = "The environment for deployment"
+}
+
+output "environment" {
+    value = var.environment
+}
