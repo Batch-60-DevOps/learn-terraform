64c545be74ff860f1c30ec8d36861372c199f2bb
sivakrishna0 <sivachowz@icloud.com>
2026-06-10 22:21:00 +0530

    robol

---
+++ Mapofmaps/var.tf
@@ -0,0 +1,33 @@
+# Demo On Map Of Maps
+variable "fruitInfo" {
+  type = map(map(string))
+  default = {
+    Apple = {
+      color = "Red"
+      taste = "Sweet"
+      price = "100"
+    }
+    Banana = {
+      color = "Yellow"
+      taste = "Sweet"
+      price = "80"
+    }
+    Lemon = {
+      color = "Yellow"
+      taste = "Sour"
+      price = "50"
+    }
+  }
+}
+
+output "fruitInfo" {
+  value = var.fruitInfo
+}
+
+output "appleInfo" {
+  value = "Apple is ${var.fruitInfo["Apple"]["color"]} in color, tastes ${var.fruitInfo["Apple"]["taste"]} and costs ${var.fruitInfo["Apple"]["price"]}"
+}
+
+output "lemonInfo" {
+  value = "Lemon is ${var.fruitInfo["Lemon"]["color"]} in color, tastes ${var.fruitInfo["Lemon"]["taste"]} and costs ${var.fruitInfo["Lemon"]["price"]}"
+}
