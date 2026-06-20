68601e092a90e21b34adcbe192f0b346402cfc24
sivakrishna0 <sivachowz@icloud.com>
2026-06-20 16:45:33 +0530

    robol

---
+++ Roboshop-V2/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "main" {
+  count        = length(var.components)
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.components[count.index]
+  }
+}
---
+++ Roboshop-V2/vars.tf
@@ -0,0 +1,12 @@
+# List Input
+variable "components" {
+    default = ["frontend", "catalogue", "shipping", "payment"]
+}
+
+variable "ami_id" {
+    default = "ami-0fcc78c828f981df2"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
