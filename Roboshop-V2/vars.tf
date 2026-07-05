e4c4dbc95c68681d2046deef92269a0d530d2659
sivakrishna0 <sivachowz@icloud.com>
2026-07-05 14:14:08 +0530

    robo-Teraform

--- Roboshop-V2/vars.tf
+++ Roboshop-V2/vars.tf
@@ -11,7 +11,7 @@ variable "components" {
         },
         "shipping" = {
             ami_id        = "ami-0fcc78c828f981df2"
-            instance_type = "t3.micro"
+            instance_type = "t3.medium"
         },
         "payment" = {
             ami_id        = "ami-0fcc78c828f981df2"
