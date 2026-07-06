c8da6315c9b8caf75a172cdfb90b38eb3519ff47
sivakrishna0 <sivachowz@icloud.com>
2026-07-06 21:23:42 +0530

    robo-Teraform-modules

---
+++ modules-V1/modules/data.tf
@@ -0,0 +1,10 @@
+data "aws_ami" "latest" {
+    most_recent = true
+    
+    filter {
+        name   = "name"
+        values = ["${var.ami_name}"] # Name of the AMI, you can find it in the AWS console or by using AWS CLI
+    }
+        
+    owners = ["self"] # Owner ID of the AMI, you can find it in the AWS console or by using AWS CLI
+}
